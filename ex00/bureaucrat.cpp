7f5cd2d88bfbc0d7a375fbd80038ef6c8a6c772f
anisdjl <adjelili954@gmail.com>
2026-05-29 18:19:22 +0200

    cpp

--- ex00/bureaucrat.cpp
+++ ex00/bureaucrat.cpp
@@ -2,28 +2,77 @@
 
 Bureaucrat::Bureaucrat(std::string name, int grade)
 {
-	if (grade > 150 || grade < 1)
-		throw ;
+	if (grade > 150)
+		throw Bureaucrat::GradeTooLowException(name, grade);
+	else if (grade < 1)
+		throw Bureaucrat::GradeTooHighException(name, grade);
 	_grade = grade;
 	_name = name;
 }
 
 Bureaucrat::~Bureaucrat(void)
 {
-	std::cout << "Bureaucrat destructor's called|n";
+	std::cout << "Bureaucrat destructor's called\n";
 }
 
-int	Bureaucrat::getGrade(void)
+int	Bureaucrat::getGrade(void) const
 {
 	return (this->_grade);
 }
 
-std::string	Bureaucrat::getName(void)
+std::string	Bureaucrat::getName(void) const
 {
 	return (this->_name);
 }
 
+void	Bureaucrat::incrementGrade(void)
+{
+	if (_grade - 1 < 1)
+		throw Bureaucrat::GradeTooHighException(_name, _grade);
+	_grade -= 1;
+}
+
+void	Bureaucrat::decrementGrade(void)
+{
+	if (_grade + 1 > 150)
+		throw Bureaucrat::GradeTooLowException(_name, _grade);
+	_grade += 1;
+}
+
 std::ostream	&operator<<(std::ostream &out, const Bureaucrat &bureau)
 {
+	out << bureau.getName() + ", bureaucrat garde " + std::to_string(bureau.getGrade()) + "\n";
 
+	return (out);
+}
+
+
+
+Bureaucrat::GradeTooHighException::GradeTooHighException(const std::string &name, int garde)
+{
+	_error = "The garde " + std::to_string(garde) + " is too high to create the bureaucrat " + name + '\n';
+}
+
+Bureaucrat::GradeTooHighException::~GradeTooHighException() throw()
+{
+}
+
+const char *Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return (_error.c_str());
+}
+
+
+Bureaucrat::GradeTooLowException::GradeTooLowException(const std::string &name, int garde)
+{
+	_error = "The garde " + std::to_string(garde) + " is too low to create the bureaucrat " + name + '\n';
+}
+
+Bureaucrat::GradeTooLowException::~GradeTooLowException() throw()
+{
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return (_error.c_str());
 }
--- ex00/bureaucrat.hpp
+++ ex00/bureaucrat.hpp
@@ -2,23 +2,44 @@
 # define BUREAUCRAT_HPP
 
 #include <iostream>
-#include <string.h>
+#include <string>
+#include <exception>
 
-class Bureaucrat : public std::exception
+class Bureaucrat
 {
 	private:
 		std::string	_name;
 		int			_grade;
 	public:
-		int	getGrade(void);
-		std::string	getName(void);
+		int			getGrade(void) const;
+		std::string	getName(void) const;
 		Bureaucrat(std::string name, int grade);
 		~Bureaucrat(void);
-		void GradeTooHighException(void);
-		void GradeTooLowException(void);
-		void incrementGrade(void); // je sais pas si il faut que je fasse un overload de l'operateur +
-		void decrementGrade(void); // pareil je ne sais pas si il faut faire un overload
-		const char *what() const noexcept override;
+		// ici je dois ajouter les constructeur de la forme canonique
+
+		void 		incrementGrade(void);
+		void 		decrementGrade(void);
+		
+		class GradeTooHighException : std::exception
+		{
+			private:
+				std::string	_error;
+			public:
+				GradeTooHighException(const std::string &name, int grade);
+				~GradeTooHighException() throw();
+				const char *what() const throw();
+		};
+
+		class GradeTooLowException
+		{
+			private:
+				std::string _error;
+			public:
+				GradeTooLowException(const std::string &name, int garde);
+				~GradeTooLowException() throw();
+				const char *what() const throw();
+		};
+		
 };
 
 std::ostream &operator<<(std::ostream &out, const Bureaucrat &bureau);
